fd6b1de38a631d4af4e87445fa93fe60c7b91e06
hifilorau <tomrau@gmail.com>
2019-08-09 20:04:29 -0400

    Updates

---
+++ precache-manifest.0c810527504b43dad75836dfc50927cf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9b298feffab4e620ae9578a5db437e6",
+    "url": "/courses/index.html"
+  },
+  {
+    "revision": "88a5088091431fcd516f",
+    "url": "/courses/static/css/main.2ba457f4.chunk.css"
+  },
+  {
+    "revision": "ac741a6b08ce48141dcd",
+    "url": "/courses/static/js/2.3ae8009d.chunk.js"
+  },
+  {
+    "revision": "88a5088091431fcd516f",
+    "url": "/courses/static/js/main.dd3bf396.chunk.js"
+  },
+  {
+    "revision": "dd78ce31a87a6f55ebfe",
+    "url": "/courses/static/js/runtime~main.5d9517cb.js"
+  },
+  {
+    "revision": "bdbeefd69c255ef08f2bf829a273a13e",
+    "url": "/courses/static/media/ctd-labs-logo.bdbeefd6.png"
+  },
+  {
+    "revision": "a3287f5d4b684c37749f4ac546d28ed9",
+    "url": "/courses/static/media/ctd-logo.a3287f5d.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/courses/static/media/logo.5d5d9eef.svg"
+  }
+]);
