8897361fb6035f2048941fb593b726a40d824f78
hifilorau <tomrau@gmail.com>
2019-08-14 16:22:33 -0400

    checkin courses route reset to half working

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,4 +1,4 @@
-export const HOME = '/';
+export const HOME = '/Courses';
 export const ABOUT  = '/Courses/about';
 export const REACT_RESOURCES="Courses/react-resources"
 export const REACT_FALL19='Courses/react_fall_2019'
